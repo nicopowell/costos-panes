e5a114e946de61366bfbd6838a1bb8bf2b47f339
Nicolas Powell <nicopowell30@gmail.com>
2024-08-25 14:50:37 -0300

    se agregaron los inputs del precio de la materia prima

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Container } from "react-bootstrap";
+import Card from "./components/Card";
 import "./css/App.css"
 
 function App() {
 
   return (
-    <>
-    </>
+      <Card/>
   );
 }
 
